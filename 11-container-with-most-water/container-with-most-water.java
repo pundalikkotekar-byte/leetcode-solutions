11bec7552cae2483796e6a9714834631b7debdad
pundalikkotekar-byte <pundalikkotekar@gmail.com>
2026-08-02 08:54:27 +0530

    Time: 5 ms (82.84%) | Memory: 77.7 MB (12.83%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0,right=height.length-1;
+        int res=0;
+
+        while(left<right)
+        {
+            int temp=Math.min(height[left] , height[right]) * (right-left);
+            res=Math.max(res,temp);
+            if(height[left] < height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }return res;
+    }
+}
