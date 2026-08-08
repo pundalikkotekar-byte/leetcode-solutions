690c4dcba3288f678e5e384bcbbe55e2c24d39a8
pundalikkotekar-byte <pundalikkotekar@gmail.com>
2026-08-08 17:16:24 +0530

    Time: 5 ms (81.23%) | Memory: 45.9 MB (53.61%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0,original=x;
+        if(x<0){
+            return false;
+        }
+        while(x!=0){
+            rev=rev*10 + (x%10);
+            x/=10;
+        }
+        if(rev==original){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
